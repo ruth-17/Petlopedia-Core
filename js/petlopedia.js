e7a983a481439ac71a57990194e025d1559f3bfa
ruth-17 <camila.alaniz116@gmail.com>
2025-04-08 20:18:11 -0300

    actualizado 8-4

---
+++ js/petlopedia.js
@@ -0,0 +1,15 @@
+function cerrarSesion() {
+    document.getElementById('boton').textContent = "Cerrar Sesión";
+}
+
+function borrarBoton() {
+    document.getElementById('btn').remove();
+}
+
+function likeCat() {
+    alert("Gato Atigrado was liked");
+}
+
+function likeDog() {
+    alert("Golden Retriever was liked");
+}
