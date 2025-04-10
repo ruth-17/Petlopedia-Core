1171051e4e98a17aa1636c0f125aeae1f9c89176
ruth-17 <camila.alaniz116@gmail.com>
2025-04-10 19:57:02 -0300

    actualizado 10-4

--- js/petlopedia.js
+++ js/petlopedia.js
@@ -6,10 +6,17 @@ function borrarBoton() {
     document.getElementById('btn').remove();
 }
 
+let contadorCat = 22;
 function likeCat() {
+    contadorCat ++;
+    document.getElementById('like-cat').textContent = `${contadorCat} me gusta`;
     alert("Gato Atigrado was liked");
 }
 
+let contadorDog = 45;
 function likeDog() {
+    contadorDog ++;
+    document.getElementById('like-dog').textContent = `${contadorDog} me gusta`;
     alert("Golden Retriever was liked");
 }
+
